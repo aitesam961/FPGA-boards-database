25b646494b5486ec6a33149764140c5a54b2365c
aitesam961 <aitesam961@gmail.com>
2025-06-14 18:11:51 +0500

    cleaup

--- docs/app.js
+++
@@ -1,7 +0,0 @@
-// Replace your entire app.js with:
-alert("Debug: Script loaded!"); // You should see this popup
-setTimeout(async () => {
-    const dbPath = 'https://aitesam961.github.io/FPGA-boards-database/fpga_boards.db';
-    const response = await fetch(dbPath);
-    alert(`Database status: ${response.status}`);
-}, 1000);
